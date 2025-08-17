ebd6b378af368bf35b9ee8d2fd537e3fb7c9e15a
Kevin JV <kevinjv0712@gmail.com>
2025-08-17 22:24:54 +0700

    Add Karasuno Players

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,6 @@
 // src/types.ts
 export type Role = "S" | "WS" | "MB" | "OP" | "LI"; // Setter, Outside, Middle, Opposite, Libero
-export type Rarity = "SP" | "UR" | "SSR" | "SR";
-export type SlotKey = "LI" | "WS1" | "WS2" | "MB1" | "MB2" | "OP" | "S";
-
-export interface Slot {
-  key: SlotKey;
-  label: string;
-  role: Role;
-}
+export type Rarity = "UR" | "SSR" | "SR";
 
 export type SkillKey =
   | "quick attack"
